239f76f77c4b8b3b80959dc50575ad2b67e4ebaa
oracle <oracle@GBTC441R11RH.montgomerycollege.edu>
2016-06-14 17:02:28 -0400

    second commit

--- src/MonopolyDice.java
+++ src/MonopolyDice.java
@@ -7,39 +7,46 @@ public class MonopolyDice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Random rnd = new Random();
+		
 		int d1 = 0;
 		int d2=0;
 		int rollcount =1;
-		int sum;
-		d1= 1+rnd.nextInt(5);
-		d2=1+rnd.nextInt(5);
-		sum=d1+d2;
+		int sum=d1+d2;
+		int diceNum=0;
 		
 		if (d1==d2){
-			System.out.println("DOUBLES!"
-					+ "\n User rolls "+d1+" & "+d2);
-			System.out.println("Move "+sum+" spaces and roll again");
-			while (rollcount<3){
+			d1= rollDice(0);
+			d2=rollDice(0);
+			System.out.println("Roll 1"
+					+ "\nDice 1: " + d1 +" Dice 2: "+d2);
+			
+			if (d1==d2){
+				d1= rollDice(0);
+				d2=rollDice(0);
+				System.out.println("Roll 2"
+						+ "\nDice 1: " + d1 +" Dice 2: "+d2);
 				if (d1==d2){
-				d1= 1+rnd.nextInt(5);
-				d2=1+rnd.nextInt(5);
-				sum=d1+d2;
-				System.out.println(" User rolls "+d1+" & "+d2);
-		System.out.println("Move "+sum+" spaces and roll stop");
-		rollcount++;
-			}
-				if (rollcount==4){
-					System.out.println("go to jail");
+					d1= rollDice(0);
+					d2=rollDice(0);
+					System.out.println("Roll 3"
+							+ "\nDice 1: " + d1 +" Dice 2: "+d2);	
 				}
-		  }
+				else{ 
+					System.out.println("Move "+sum+" spaces and stop");
+			 }
+			}
+			else{ 
+				System.out.println("Move "+sum+" spaces and stop");
+			}
+			
 		}
-		else {
-			System.out.println("User rolls " +d1+" & "+d2);
-			System.out.println("Move "+sum+" spaces and stop");
-		}
+			
+	}
+	public static int rollDice(int diceNum){
+		Random rnd = new Random();
+		 diceNum = 1+rnd.nextInt(5);
 		
-		
+		return  diceNum;
 	}
 
 }
